022d06f5a2fe574ae8306bdbf50588093a323c3e
Isaac Noriega <isaac.noriega@iteso.mx>
2024-03-14 21:46:57 -0600

    Uso de las rutas de user y de express.json para las peticiones

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
-import { Router } from "express";
+import express,{ Router } from "express";
+import user from './users';
 
 const router = Router();
+
+router.use(express.json());
+router.use(user);
 
 router.get('', (req , res)=>{
     res.send('Api is working');
