87e6538b5fd5195a2f0027d2d6e92a1389b555e8
Isaac Noriega <isaac.noriega@iteso.mx>
2024-03-14 21:45:22 -0600

    Correcion y cambio de nombre para el registro del usuario

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,50 +1,26 @@
 import { Request, Response } from "express";
 import ResponseStatus from '../utils/response-status';
 import hashPassword from "../utils/hash-password";
-import User, { IUser } from '../models/user.model';
+import User from '../models/user.model';
 
 class UsersController {
-    registerUser(req: Request, res: Response): void {
-        try {
-            // Obtener los datos del formulario
-            const { email, name, password } = req.body;
 
-            // Hashear la contraseña antes de guardarla en la base de datos
-            const hashedPassword: string = hashPassword(password);
-
-            // Crear una nueva instancia del modelo de usuario con los datos del formulario
-            const newUser: IUser = new User({
-                name: name,
-                email: email,
-                password: hashedPassword,
-            });
-
-            // Guardar el nuevo usuario en la base de datos
-            newUser.save((error: any, savedUser: IUser | null) => {
-                if (error) {
-                    console.error('Error al guardar usuario:', error);
-                    res.status(ResponseStatus.BAD_REQUEST).json({ error: 'Error al crear usuario' });
-                } else if (savedUser) {
-                    // Usuario guardado correctamente
-                    const data = {
-                        name: savedUser.name,
-                        email: savedUser.email,
-                    };
-                    res.status(ResponseStatus.SUCCESS).redirect('/'); // Redirigir al usuario después de registrar
-                } else {
-                    // Manejar el caso en el que savedUser es null
-                    console.error('El usuario guardado es nulo');
-                    res.status(ResponseStatus.INTERNAL_SERVER_ERROR).send('Error al crear usuario');
-                }
-            });
-
-        } catch (error) {
-            // Manejar errores
-            console.error('Error al registrar usuario:', error);
-            res.status(500).send('Error al registrar usuario');
-        }
+    //Post para crear usuario
+    signUp(req: Request, res: Response): void {
+        //Datos del usuario
+        const data = {
+            name : req.body.name,
+            email : req.body.email,
+            password : hashPassword(req.body.password)
+        };
+        //Crear el nuevo usuario en la DB
+        User.create(data).then( response => {
+            res.status(ResponseStatus.SUCCES).send('Sign Up success '); 
+        }).catch( e => {
+            res.status(ResponseStatus.BAD_REQUEST).send('Something went wrong');        
+        });
     }
 }
 
-export default UsersController;
+export default new UsersController();
 
