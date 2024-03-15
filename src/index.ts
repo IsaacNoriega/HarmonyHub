f7a13d50fc408cf6396e9b6fd26bae8717318a20
DESKTOP-03NAKMJ\Mateo <matehdz150@gmail.com>
2024-03-15 09:58:47 -0600

    Handlebars y register

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import dotenv from 'dotenv'
 import express from 'express';
 import routes from './routes/index'
 import mongoose from 'mongoose';
+import path from 'path';
+import { engine } from 'express-handlebars';
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
-const db_url = process.env.URLDB;
+const db_url = process.env.URLDB || 'mongodb+srv://mateeldemoledor:hola123@cluster0.ztfvxtn.mongodb.net/HarmonyHub?retryWrites=true&w=majority';
 
 const app = express();
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views','./src/views')
+
+app.use('/assets',express.static(path.join(__dirname,'public')));
 app.use(routes);
 
+console.log(db_url);
 mongoose.connect(db_url).then(() => {
     app.listen(port , ()=>{
         if(process.env.NODE_ENV == 'dev'){
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,13 @@ import userController from "../controllers/user.controller";
 
 const router = Router();
 
+router.get("/signup", (req, res) => {
+    res.render('register');
+
+});
+
 router.post('/signup', userController.signUp);
+
+
 
 export default router;
