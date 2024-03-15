7bb7d948bf0db1d0cc17bffa831a747d8f6d99c9
Isaac Noriega <isaac.noriega@iteso.mx>
2024-03-14 21:47:54 -0600

    Correcion de la coneccion de la DB y el uso de dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,24 @@
+import dotenv from 'dotenv'
 import express from 'express';
 import routes from './routes/index'
 import mongoose from 'mongoose';
 
-const app = express();
+dotenv.config();
 
 const port = process.env.PORT || 3000;
+const db_url = process.env.URLDB;
 
+const app = express();
 app.use(routes);
 
-const db_url = process.env.URLDB;
-console.log(db_url);
-async function connect() {
-    try {
-        await mongoose.connect(db_url);
-        app.listen(port, () => {
-            if (process.env.NODE_ENV == 'dev') {
-                console.log('App is running in port ' + port)
-            } else {
-                console.log('App running ' + port);
-            }
-
-        });
-    } catch (e) {
-        console.log('failed to connect to db ', e);
-    }
-}
-
-connect();
+mongoose.connect(db_url).then(() => {
+    app.listen(port , ()=>{
+        if(process.env.NODE_ENV == 'dev'){
+            console.log('App is running on port' , port)
+        }else{
+            console.log('App running',port)
+        }
+    })
+}).catch( e => {
+    console.log('failed to connect to db ', e);
+});
