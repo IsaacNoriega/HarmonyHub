23d08382d807f4149e1f1b31ad8af63ffba71928
Isaac Noriega <isaac.noriega@iteso.mx>
2024-04-04 14:08:07 -0600

    Se creo un archivo para la configuracion del storage

---
+++ src/middlewares/upload-s3.ts
@@ -0,0 +1,30 @@
+import multer , {FileFilterCallback} from "multer";
+import multerS3 from 'multer-s3';
+import { S3Client } from "@aws-sdk/client-s3";
+
+//Conexion al bucket 
+const s3 = new S3Client({
+    region : process.env.REGION,
+    credentials : {
+        accessKeyId : process.env.S3_ACCESS_KEY,
+        secretAccessKey : process.env.S3_SECRET_KEY
+    }
+});
+
+const s3Storage = multerS3({
+    s3 : s3,
+    bucket : process.env.S3_NAME,
+    metadata : (req, file, cb) => {
+        cb(null,{...file});
+    },
+    acl : 'public-read' , 
+    key : (req ,file , cb)=>{
+        cb(null,file.originalname);
+    }
+});
+
+const upload = multer({
+    storage : s3Storage
+});
+
+export default upload;
