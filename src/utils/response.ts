cd5186a0611462426d0ff513bc9a0fa4e120efdc
Isaac Noriega <isaac.noriega@iteso.mx>
2024-04-25 19:34:00 -0600

    Respuestas del chat

---
+++ src/utils/response.ts
@@ -0,0 +1,6 @@
+export default {
+    HELLO : `Hola ¿En que puedo ayudarte? <br> 1) Cuenta <br> 2) Carga de archivos <br>3)Otros`,
+    OPT1 : ` ¿Qué necesitas saber de tu cuenta? <br> 1)Correo <br>2)Contraseña <br>3)Foto`,
+    OPT2 : `Lo siento aun estamos trabajando en ello`,
+    OPT3 : 'Para consultar otro tema favor de mandarnos un correo a HarmonyHub-Support@outlook.com'
+}
