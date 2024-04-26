c4c7371e87d9e6cb2c03ec275a55af2e2e8d3b29
Isaac Noriega <isaac.noriega@iteso.mx>
2024-04-25 19:33:33 -0600

    ruta /support para ir a support

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import express,{ Router } from "express";
 import user from './users';
 import authGoogle from './authGoogle'
+import path from 'path'
 
 
 const router = Router();
@@ -11,6 +12,10 @@ router.use('/google', authGoogle);
 
 router.get('', (req , res)=>{
     res.send('Api is working');
+});
+
+router.get('/support', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 export default router;
