ce9fd64305ccd27331b51f0090bb07562a724ac6
Isaac Noriega <isaac.noriega@iteso.mx>
2024-04-30 19:18:03 -0600

    Se reconfiguro para que sea una sala diferente para usuario

--- src/controllers/support.ts
+++ src/controllers/support.ts
@@ -4,46 +4,48 @@ import response from '../utils/response';
 export default function setupSocket(server) {
     const io = new socketIo.Server(server);
 
-    // Configura Socket.IO para la ruta '/support'
-    io.of('/support').on('connection', (socket) => {
+    io.on('connection', (socket) => {
         console.log('Un nuevo usuario se ha conectado');
         
         socket.on('newUser', (data) => {
-            socket.data.user = data.user;
-            socket.broadcast.emit('newUser', data);
-            socket.emit('chat message', { user: 'Soporte', message: `Hola ${data.user}, bienvenido al chat de Soporte de HarmonyHub. Para iniciar la conversacion escribe "Hola" y para terminar escriba "Adios" `});
+            // Asignar al usuario una sala de chat única basada en su identificador (p. ej., nombre de usuario)
+            socket.join(data.user);
+
+            // Emitir un mensaje de bienvenida al usuario
+            io.to(data.user).emit('chat message', { user: 'Soporte', message: `Hola ${data.user}, bienvenido al chat de Soporte de HarmonyHub. Para iniciar la conversación, escribe "Hola" y para terminar, escribe "Adiós"` });
         });
 
         socket.on('newMessage', (data) => {
-
             let responseMessage = '';
-            // Verifica el mensaje recibido y asigna la respuesta correspondiente
+
             switch (data.message.toLowerCase()) {
                 case 'hola':
-                    responseMessage = response.HELLO
+                    responseMessage = response.HELLO;
                     break;
                 case 'adios':
-                    responseMessage = response.BYE
+                    responseMessage = response.BYE;
                     break;
                 case '1':
-                    responseMessage = response.OPT1
+                    responseMessage = response.OPT1;
                     break;
                 case '2':
-                    responseMessage = response.OPT2
+                    responseMessage = response.OPT2;
                     break;
                 case '3':
-                    responseMessage = response.OPT3
+                    responseMessage = response.OPT3;
                     break;
                 default:
-                    responseMessage = response.ERROR
+                    responseMessage = response.ERROR;
                     break;
             }
-            // Emitir el mensaje de respuesta solo al cliente que envió el mensaje
-            socket.emit('chat message', { user: 'Server', message: responseMessage });
+
+            // Enviar el mensaje de respuesta solo a la sala de chat del usuario que envió el mensaje
+            io.to(data.user).emit('chat message', { user: 'Server', message: responseMessage });
         });
         
         socket.on('disconnect', () => {
-            socket.broadcast.emit('userLeft', { ...socket.data });
+            // Aquí puedes implementar la lógica para manejar la desconexión de un usuario
+            console.log('Usuario desconectado');
         });
     });
 }
