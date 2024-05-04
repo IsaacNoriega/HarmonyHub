2b9098cc72cc8da01f6e195d7727cfa5f25d3e49
Isaac Noriega <isaac.noriega@iteso.mx>
2024-05-04 17:35:51 -0600

    Agregue 2 campos a songs (name y url)

--- src/models/projects.model.ts
+++ src/models/projects.model.ts
@@ -3,19 +3,22 @@ import { Schema, model, Document } from 'mongoose';
 export interface IUser extends Document {
     projectName: string;
     userId: string;
-    songs: string[];
-    projectImage : string;
+    songs: { name: string, url: string }[];
+    projectImage: string;
     collaborators: string[];
 }
 
 const userSchema = new Schema({
     projectName: { type: String, required: true },
     userId: { type: String, required: true },
-    songs: [{type: String, required: false }],
-    ProjectImage : { type : String, required: false},
+    songs: [{
+        name: { type: String, required: true },
+        url : { type: String, required : true}
+    }],
+    projectImage : { type : String, required: false},
     collaborators: [{type: String, required: false}]
 });
 
-const User = model('project', userSchema);
+const User = model<IUser>('project', userSchema);
 
 export default User;
