fb44cbd180b822aa6e0b254ef4514db2028bfbe9
Isaac Noriega <isaac.noriega@iteso.mx>
2024-05-06 18:02:09 -0600

    Se modifico la solicitud de ajax para q obtenga el file

--- src/public/js/projects-section.js
+++ src/public/js/projects-section.js
@@ -44,58 +44,62 @@ document.addEventListener('click', function (event) {
 });
 
 
-//crear nuevo proyecto
 document.getElementById('projectForm').addEventListener('submit', function (event) {
-
   event.preventDefault();
 
+  // Obtener los datos del formulario
   const projectName = document.getElementById('inputNewProject').value;
-  // Esto imprimirá el valor del userId que viene del contexto de Handlebars
+  const fileInput = document.querySelector('input[type="file"]');
+  const file = fileInput.files[0]; // Obtener el primer archivo seleccionado
+  const fileUrl = `https://harmonyhub.s3.us-east-2.amazonaws.com/${file.name}`;
+  const userId = userIdFront; // Supongo que userIdFront ya está definido en tu código
 
-  const data = {
-    projectName: projectName,
-    userId: userIdFront
-  };
+  // Crear un objeto FormData para enviar los datos con el archivo
+  const formData = new FormData();
+  formData.append('Foto', file); // Añadir el archivo al FormData
+  formData.append('projectName', projectName); // Añadir el nombre del proyecto
+  formData.append('userId', userId); // Añadir el userId
 
   // Enviar solicitud POST utilizando AJAX
   $.ajax({
-    url: 'http://localhost:3000/home/createProject',
-    type: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify(data),
-    success: (response) => {
-      const cardsContainer = document.getElementById('cardsContainer');
-      const child = cardsContainer.firstElementChild;
-      const newDiv = document.createElement('div');
-      newDiv.classList.add('card');
-      newDiv.innerHTML = `
-          <div class="card-image">
-            <img src="https://i.pinimg.com/564x/47/5b/72/475b72acb3f65dd57e938e4eab8ac895.jpg" alt="">
-          </div>
-          <div class="card-description">
-            <p class="text-title">${data.projectName}</p>
-            <p class="text-body">By ${data.userId}</p>
-      `;
+      url: 'http://localhost:3000/home/createProject',
+      type: 'POST',
+      data: formData, // Enviar FormData en lugar de JSON
+      contentType: false, // Importante: no configurar contentType cuando se usa FormData
+      processData: false, // Importante: no procesar datos cuando se usa FormData
+      success: function (response) {
+          // Manejar el éxito de la solicitud
+          const cardsContainer = document.getElementById('cardsContainer');
+          const child = cardsContainer.firstElementChild;
+          const newDiv = document.createElement('div');
+          newDiv.classList.add('card');
+          newDiv.innerHTML = `
+              <div class="card-image">
+                  <img src="${fileUrl}" alt="">
+              </div>
+              <div class="card-description">
+                  <p class="text-title">${projectName}</p>
+                  <p class="text-body">By ${userId}</p>
+              </div>
+          `;
 
-      if (child.classList.contains('noProjects')) {
-        cardsContainer.innerText = '';
-        cardsContainer.appendChild(newDiv);
-      } else {
-        cardsContainer.appendChild(newDiv);
+          if (child.classList.contains('noProjects')) {
+              cardsContainer.innerText = '';
+              cardsContainer.appendChild(newDiv);
+          } else {
+              cardsContainer.appendChild(newDiv);
+          }
+
+          const modalProject = document.getElementById('modalNewProject');
+          modalProject.style.display = 'none';
+      },
+      error: function (xhr, status, error) {
+          // Manejar errores de la solicitud
+          console.error('Error en la solicitud:', status, error);
       }
-
-      const modalProject = document.getElementById('modalNewProject');
-      modalProject.style.display = 'none';
-
-
-    },
-    error: function (xhr, status, error) {
-      // Manejar errores de la solicitud
-      console.error('Error en la solicitud:', status, error);
-    }
   });
-
 });
+
 
 function redirectProject(projectName, userId) {
   $.ajax({
