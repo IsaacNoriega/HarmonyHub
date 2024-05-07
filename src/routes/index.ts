09701341de88d99532aa9b320bd2344adbb03040
Isaac Noriega <isaac.noriega@iteso.mx>
2024-05-06 18:04:21 -0600

    Añadi el endpoint de logout

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import authGoogle from './authGoogle';
 import path from 'path';
 import home from './home';
 import project from './project';
+import responseStatus from "../utils/response-status";
 
 
 const router = Router();
@@ -21,5 +22,19 @@ router.get('', (req , res)=>{
 router.get('/support', (req, res) => {
     res.sendFile(path.join(__dirname, '../public', 'support.html'));
 });
+
+router.post('/logout' , ( req , res) => {
+    const cookiesToDelete = ['username', 'token', 'tokenProject', 'email', 'connect.sid'];
+    req.session.destroy( (err) =>{
+        if(err){
+            res.status(responseStatus.BAD_REQUEST).send(err);
+        }else {
+            cookiesToDelete.forEach(cookieName => {
+                res.clearCookie(cookieName);
+            });
+            res.redirect('/login')
+        }
+    })
+})
 
 export default router;
